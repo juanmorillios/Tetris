4cbc8eb4102e33c0eea198b73aa5cd4e2925eb52
Juan Ml. Morillo <juanmorillios@gmail.com>
2014-11-01 22:52:25 +0100

    Block
    Create Block

--- Tetris/Block.swift
+++ Tetris/Block.swift
@@ -7,3 +7,62 @@
 //
 
 import Foundation
+
+import SpriteKit
+
+
+
+let numberOfColors: UInt32 = 6
+
+
+enum BlockColor: Int, Printable {
+
+
+
+    case Blue = 0, Orange, Purple, Red, Teal, Yellow
+    
+    
+    var spriteName: String {
+    
+        switch self {
+        
+        case .Blue:
+            return "blue"
+        case .Orange:
+            return "orange"
+        case .Purple:
+            return "purple"
+        case .Red:
+            return "red"
+        case .Teal:
+            return "teal"
+        case .Yellow:
+            return "yellow"
+            
+        
+        
+        }
+    
+
+    }
+
+    
+    var description: String {
+    
+    
+        return self.spriteName
+    
+    
+    }
+    
+    
+    static func random() -> BlockColor  {
+    
+    
+        return BlockColor(rawValue: Int(arc4random_uniform(numberOfColors)))!
+    
+    
+    }
+
+
+}
